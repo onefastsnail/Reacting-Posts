c88a6f19d7006e40412c533926852d86c93409df
Paul Stewart <onefastsnail@gmail.com>
2017-05-01 09:45:27 +0300

    Added a state toggle example for toggling post content in the post component

--- assets/react/components/Posts/Post.js
+++ assets/react/components/Posts/Post.js
@@ -6,11 +6,17 @@ class Post extends React.Component {
   constructor(props) {
     //this calls the parent constructor
     super(props);
+
+    this.state = {
+      showPostContent: false
+    };
+
+    this.handleShowContent = this.handleShowContent.bind(this);
   }
 
   //a method to handle our clicks
-  handleClick(item) {
-    console.log(item); // null
+  handleShowContent() {
+    this.setState({'showPostContent': !this.state.showPostContent});
   }
 
   //Our method used by react, and is required for components
@@ -20,20 +26,26 @@ class Post extends React.Component {
       backgroundImage: 'url(http://fakeimg.pl/650x650/eeeeee/666/?text='+this.props.data.id+')'
     };
 
+    var postContent;
+
+    if(this.state.showPostContent){
+      postContent = <div className="c-wysiwyg-html" dangerouslySetInnerHTML={{__html: this.props.data.post_content}}></div>;
+    }
+
     return(
 
       <div className="c-small-post">
 
           <div className="c-small-post__image">
-              <a href={this.props.data.link} title="{this.props.data.post_title}" className="h-full-cover"><div className="c-background-image" style={heroBackgroundStyles}></div></a>
+              <a href="javascript:;" title="{this.props.data.post_title}" className="h-full-cover" onClick={this.handleShowContent}><div className="c-background-image" style={heroBackgroundStyles}></div></a>
           </div>
 
           <div className="c-small-post__content">
 
-            <p className="c-small-post__date">{this.props.data.date}</p>
-
-              <h3 className="c-small-post__title"><a href={this.props.data.link} title="{this.props.data.post_title}">{this.props.data.post_title}</a></h3>
+              <h3 className="c-small-post__title"><a href="javascript:;" title="{this.props.data.post_title}" onClick={this.handleShowContent}>{this.props.data.post_title}</a></h3>
               <p><small>{this.props.data.post_date}</small></p>
+
+              {postContent}
 
           </div>
 
